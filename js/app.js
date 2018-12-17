092ece2164da5a48b65cbe331158ad5bd96b170f
KubaMikolajczyk <qba600@gmail.com>
2018-12-17 17:21:05 +0100

    Add clearing fields after adding an element

--- js/app.js
+++ js/app.js
@@ -100,6 +100,19 @@ var UIController = (function(){
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function(){
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -140,6 +153,9 @@ var controller = (function(budgetCtrl, UICtrl){
 
     //Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+
+    //clear the fields
+    UICtrl.clearFields();
 
     // Calculate the budget
 
