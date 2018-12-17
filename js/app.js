caceca79b8b7a81042b374cc45f181f42a68a351
KubaMikolajczyk <qba600@gmail.com>
2018-12-17 13:21:03 +0100

    arrange class names to an obj

--- js/app.js
+++ js/app.js
@@ -8,13 +8,24 @@ var budgetController = (function(){
 // UI CONTROLLER
 var UIController = (function(){
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  }
+
   return{
     getInput: function(){
       return{
-        type: document.querySelector('.add__type').value, // Will be either inc or exp
-        description: document.querySelector('.add__description').value,
-        value: document.querySelector('.add__value').value
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    getDOMstrings: function(){
+      return DOMstrings;
     }
   };
 
@@ -23,6 +34,8 @@ var UIController = (function(){
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function(){
 
@@ -40,7 +53,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
 
