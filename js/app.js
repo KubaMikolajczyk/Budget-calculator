ef373c1a18512c8c77376d34ec311b7ca1e6e581
KubaMikolajczyk <qba600@gmail.com>
2018-12-19 15:05:20 +0100

    add budget calculating in model controller

--- js/app.js
+++ js/app.js
@@ -13,6 +13,13 @@ var budgetController = (function(){
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur){
+      sum = sum + cur.value;
+    });
+    data.totals[type] = sum;
+  };
 
   var data = {
     allItems: {
@@ -22,8 +29,14 @@ var budgetController = (function(){
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
+
+
+
+
 
   return{
     addItem: function(type, des, val){
@@ -49,6 +62,33 @@ var budgetController = (function(){
 
       //return the new element
       return newItem;
+    },
+
+    calculateBudget: function() {
+
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      //calculate the budget : income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      //calculate the percentage of income that we spend
+      if(data.totals.inc > 0){
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+    },
+
+    getBudget: function(){
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
     },
 
     testing: function() {
@@ -143,11 +183,13 @@ var controller = (function(budgetCtrl, UICtrl){
   var updateBudget = function() {
 
     // Calculate the budget
+    budgetCtrl.calculateBudget();
 
     //return the budget
+    var budget = budgetCtrl.getBudget();
 
     //Display the budget on the UI
-
+    console.log(budget);
 
   };
 
