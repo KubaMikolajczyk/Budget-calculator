f177d98ded056e03ab99dd2f50abe3dda426d77d
KubaMikolajczyk <qba600@gmail.com>
2019-01-03 20:36:28 +0100

    add displaying percentages

--- js/app.js
+++ js/app.js
@@ -159,7 +159,8 @@ var UIController = (function(){
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   }
 
   return{
@@ -222,7 +223,30 @@ var UIController = (function(){
         document.querySelector(DOMstrings.percentageLabel).textContent = '---';
       }
     },
-
+    
+    displayPercentages: function(percentages) { 
+    
+        var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+        
+        var nodeListForEach = function(list, callback) {
+            for (var i = 0; i < list.length; i++) {
+                callback(list[i], i);
+            }
+        };
+        
+        nodeListForEach(fields, function(current, index) {
+            
+            if (percentages[index] > 0) {
+                current.textContent = percentages[index] + '%';
+            } else {
+                current.textContent = '---';
+            }
+            
+            
+        });
+        
+    },
+    
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -261,7 +285,7 @@ var controller = (function(budgetCtrl, UICtrl){
       var percentages = budgetCtrl.getPercentages();
       
       // update the UI with new percentages
-      console.log(percentages);
+      UICtrl.displayPercentages(percentages);
   };
 
   var updateBudget = function() {
