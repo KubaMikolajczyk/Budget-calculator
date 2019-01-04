68a57cca1c47302c4ba7813224544a6f39f9fdc9
KubaMikolajczyk <qba600@gmail.com>
2019-01-04 15:30:24 +0100

    Update actual date to UI

--- js/app.js
+++ js/app.js
@@ -160,7 +160,8 @@ var UIController = (function(){
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   }
   
   var formatNumber = function(num, type) {
@@ -270,6 +271,20 @@ var UIController = (function(){
             
             
         });
+        
+    },
+      
+    displayMonth: function() {
+        var now, year, month, months;
+      
+        now = new Date();
+        
+        year = now.getFullYear();
+        month = now.getMonth();
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        month = months[month]
+        
+        document.querySelector(DOMstrings.dateLabel).textContent = month + ' ' + year;
         
     },
     
@@ -384,6 +399,7 @@ var controller = (function(budgetCtrl, UICtrl){
   return{
     init: function() {
       console.log("App has started.");
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
