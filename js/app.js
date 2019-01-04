4f727b5f2609d60afcd1b4f10c99f2a31609d609
KubaMikolajczyk <qba600@gmail.com>
2019-01-04 17:22:03 +0100

    add UX improvement/ color change

--- js/app.js
+++ js/app.js
@@ -187,6 +187,12 @@ var UIController = (function(){
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
         
     };
+    
+  var nodeListForEach = function(list, callback) {
+            for (var i = 0; i < list.length; i++) {
+                callback(list[i], i);
+            }
+        };
 
   return{
     getInput: function(){
@@ -255,11 +261,7 @@ var UIController = (function(){
     
         var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
         
-        var nodeListForEach = function(list, callback) {
-            for (var i = 0; i < list.length; i++) {
-                callback(list[i], i);
-            }
-        };
+        
         
         nodeListForEach(fields, function(current, index) {
             
@@ -286,6 +288,20 @@ var UIController = (function(){
         
         document.querySelector(DOMstrings.dateLabel).textContent = month + ' ' + year;
         
+    },
+      
+    changedType: function() {
+      
+        var fields = document.querySelectorAll(
+            DOMstrings.inputType + ',' +
+            DOMstrings.inputDescription + ',' +
+            DOMstrings.inputValue);
+        
+        nodeListForEach(fields, function(cur){
+            cur.classList.toggle('red-focus');
+        });
+        
+        document.querySelector(DOMstrings.inputButton).classList.toggle('red');
     },
     
     getDOMstrings: function(){
@@ -315,6 +331,8 @@ var controller = (function(budgetCtrl, UICtrl){
     });
       
       document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+      
+      document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
     
   var updatePercentages = function() {
